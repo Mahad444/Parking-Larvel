82cf52475ffc61cf0c6bd3588d68ae8d86c7dd8f
Ally Juma Said <mahadsaid95@icloud.com>
2023-12-12 10:13:06 +0000

    Add PlaceController and UserController with necessary methods

--- app/Http/Controllers/PlaceController.php
+++ app/Http/Controllers/PlaceController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
+
 use App\Models\Place;
 
 use Illuminate\Http\Request;
@@ -29,8 +31,8 @@ class PlaceController extends Controller
 
         $place->update([
             'user_id' => $request->user_id,
-            'start_time' => $request->start_time,
-            available => 0,
+            'start_time' => now(),
+            'available' => 0,
             'end_time' => null,
             'total_price' => null,
         ]);
@@ -39,4 +41,41 @@ class PlaceController extends Controller
 
         return PlaceResource::make($place);
     }
+
+
+    // end parking session
+
+    public function endParking(Place $place)
+
+    {
+    
+        $place->update([
+            'available' => 1,
+            'end_time' => now(),
+            'total_price' => $this->calculatePrice($place->sector_id, $place->start_time),
+        ]);
+
+
+        return PlaceResource::make($place);
+    }
+
+    //  calculate price 
+
+    public function calculatePrice($sector_id, $start_time)
+
+    {
+        $tart = Carbon::createMidnightDate($start_time);
+        $end = Carbon::createMidnightDate(now());
+        $totalDuration = $start->diffInHours($end);
+        $sector_hourly_price = Sector::find($sector_id)->hourly_price;
+
+
+       if ($totalDuration > 1) {
+        return ceil($sector_hourly_price * $totalDuration);
+       }
+    //    OR
+       return $sector_hourly_price;
+
+    }
+
 }
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Hash;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    // Storing info
+     public function store(Request $request)
+
+     {
+            $request -> validate([
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' => ['required', 'string', 'min:8', 'confirmed'],
+            ]);
+
+            // user 
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+
+            // token
+            return response()->json([
+                'token' => $user->createToken('auth_token')->plainTextToken,
+                'token_type' => 'Bearer',
+            ]);    
+
+        }
+
+        // auth
+        public function auth(Request $request)
+        {
+            $request->validate([
+                'email' => ['required', 'email' , 'string', 'max:255'],
+                'password' => ['required', 'string','min:8'],
+            ]);
+
+            $user = User::whereEmail('email', $request->email)->first();
+
+            if (! $user || ! Hash::check($request->password, $user->password)) {
+                throw ValidationException::withMessages([
+                    'email' => ['The provided credentials are incorrect.'],
+                ]);
+            }
+
+            return response()->json([
+                'token' => $user->createToken('auth_token')->plainTextToken,
+                'token_type' => 'Bearer',
+            ]);
+        }
+
+        // logout
+        public function logout(Request $request)
+        {
+            $request->user()->currentAccessToken()->delete();
+
+            return response()->noContent();
+
+            // return response()->json(['message' => 'Logged out']);
+        }
+}

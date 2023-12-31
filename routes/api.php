35b9bd26ca5159bd367d6dd0f050eecde7ecd927
Ally Juma Said <mahadsaid95@icloud.com>
2023-12-31 17:28:03 -0500

    updating api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SectorController;
+use App\Http\Controllers\PlaceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +18,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('user', function (Request $request) {
+        return [
+            'user' => $request->user(),
+            'currentToken' => $request->bearerToken()
+        ];    
+    });
+
+    Route::put('user/update/profile', [ProfileController::class,'updateUserInfo']);
+    Route::put('user/update/password', [ProfileController::class,'updateUserPassword']);
+    Route::post('user/logout', [UserController::class,'logout']);
+    Route::get('user/sectors', [SectorController::class,'index']);
+    Route::put('parking/{place}/start', [PlaceController::class,'startParking']);
+    Route::put('parking/{place}/end', [PlaceController::class,'endParking']);
 });
+
+Route::post('user/register', [UserController::class,'store']);
+Route::post('user/login', [UserController::class,'auth']);
